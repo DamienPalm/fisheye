e2f968443d4cf14c6e397af48c699e9383896efe
Damien Palm <palm.damien.pro@gmail.com>
2024-07-30 23:51:17 +0200

    conversion logo.png to logo.webp

--- js/components/headerHomepage.js
+++ js/components/headerHomepage.js
@@ -2,7 +2,7 @@ const render = () => {
   return `
     <header class="header">
       <a href="./index.html" class="header__link">
-        <img src="./assets/images/logo.png" alt="Fisheye Home page" class="header__link__logo">
+        <img src="./assets/images/logo.webp" alt="Fisheye Home page" class="header__link__logo">
       </a>
       <h1 class="header__title">Nos photographes</h1>
     </header>
