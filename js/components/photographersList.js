03ef148bf1ae486a5a1298509b11ceae3d498d0f
Damien Palm <palm.damien.pro@gmail.com>
2024-07-31 15:38:28 +0200

    account.png with miss thumbnail

--- js/components/photographersList.js
+++ js/components/photographersList.js
@@ -5,12 +5,20 @@ const render = (element) => {
     .map(
       (element) => `
     <article class="main__card">
-      <a class="main__card__head-card" href="../photographer.html?id=${element.id}">
-        <img src="${element.portrait_thumbnail}" alt="${element.name}" class="main__card__head-card__photo">
+      <a class="main__card__head-card" href="../photographer.html?id=${
+        element.id
+      }">
+        <img src="${
+          element.portrait_thumbnail
+            ? element.portrait_thumbnail
+            : "./assets/photographers/account.png"
+        }" alt="${element.name}" class="main__card__head-card__photo">
         <h2 class="main__card__head-card__title">${element.name}</h2>
       </a>
       <div class="main__card__body-card">
-        <p class="main__card__body-card__location">${element.city}, ${element.country}</p>
+        <p class="main__card__body-card__location">${element.city}, ${
+        element.country
+      }</p>
         <p class="main__card__body-card__tagline">${element.tagline}</p>
         <p class="main__card__body-card__price">${element.price}€/jour</p>
       </div>
