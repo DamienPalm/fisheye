47c3aca341976c82215253007b03130e4d7438c2
Damien Palm <palm.damien.pro@gmail.com>
2024-08-07 01:58:26 +0200

    modification filte

--- js/components/filterMedia.js
+++ js/components/filterMedia.js
@@ -1,17 +1,48 @@
 const render = () => {
   return `
-    <div class="main__media-section__order-by">
-        <label class="main__media-section__order-by__label" for="order-by"><p class="main__media-section__order-by__label__text">Trier par</p></label>
-        <select class="main__media-section__order-by__select" name="Order by" id="order-by">
-            <option class="main__media-section__order-by__select__option" value="popularity">Popularité</option>
-            <option class="main__media-section__order-by__select__option" value="date">Date</option>
-            <option class="main__media-section__order-by__select__option" value="title">Titre</option>
-        </select>
-    </div>
-    `;
+    <section class="filter-section">
+      <h2>Trier par</h2>
+      <div class="dropdown">
+        <div class="select">
+          <span class="selected">Popularité</span>
+          <i class="fa-solid fa-chevron-down chevron-down"></i>
+        </div>
+        <ul class="menu">
+          <li class="list active">Popularité</li>
+          <li class="list">Date</li>
+          <li class="list">Titre</li>
+        </ul>
+      </div>
+    </section>
+  `;
 };
 
-const event = () => {};
+const event = () => {
+  const select = document.querySelector(".select");
+  const chevron = document.querySelector(".chevron-down");
+  const menu = document.querySelector(".menu");
+  const options = document.querySelectorAll(".list");
+  const selected = document.querySelector(".selected");
+
+  select.addEventListener("click", () => {
+    select.classList.toggle("selected-clicked");
+    chevron.classList.toggle("chevron-rotate");
+    menu.classList.toggle("open-menu");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      selected.innerText = option.innerText;
+      select.classList.remove("selected-clicked");
+      chevron.classList.remove("chevron-rotate");
+      menu.classList.remove("open-menu");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      option.classList.add("active");
+    });
+  });
+};
 
 export default {
   render,
