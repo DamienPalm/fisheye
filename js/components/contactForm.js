74be8397b74c2cbf860e3879146d2c5c5b36c5de
Damien Palm <palm.damien.pro@gmail.com>
2024-08-07 01:57:37 +0200

    contact form creation

--- js/components/contactForm.js
+++ js/components/contactForm.js
@@ -1,22 +1,34 @@
 import { closeModal } from "../utils/contactForm.js";
 
-const render = () => {
+const render = (element) => {
   return `
-    <div id="contact_modal">
-    <div class="modal">
-      <header>
-        <h2>Contactez-moi</h2>
-        <img src="./assets/icons/close.svg" id="closeButton">
-      </header>
-      <form>
-        <div>
-          <label>Prénom</label>
-          <input />
-        </div>
-        <button class="contact_button">Envoyer</button>
-      </form>
+    <div class="modalBackground" id="contactModal">
+      <div class="modalBackground__modal">
+        <header class="modalBackground__modal__header">
+          <h2 class="modalBackground__modal__header__title">Contactez-moi ${element.name}</h2>
+          <img class="modalBackground__modal__header__closeButton" src="./assets/icons/close.svg" id="closeButton">
+        </header>
+        <form action="#" method="post" class="modalBackground__modal__form" name="contact-me">
+          <div class="modalBackground__modal__form__formData">
+            <label class="modalBackground__modal__form__formData__label" for="firstName">Prénom</label>
+            <input type="text" class="modalBackground__modal__form__formData__input" id="firstName" name="firstName">
+          </div>
+          <div class="modalBackground__modal__form__formData">
+            <label class="modalBackground__modal__form__formData__label" for="lastName">Nom</label>
+            <input type="text" class="modalBackground__modal__form__formData__input" id="lastName" name="lastName">
+          </div>
+          <div class="modalBackground__modal__form__formData">
+            <label class="modalBackground__modal__form__formData__label" for="email">Email</label>
+            <input type="email" class="modalBackground__modal__form__formData__input" id="email" name="email" autocomplete="off">
+          </div>
+          <div class="modalBackground__modal__form__formData">
+            <label class="modalBackground__modal__form__formData__label" for="yourMessage">Votre message</label>
+            <textarea class="modalBackground__modal__form__formData__textarea" id="yourMessage" name="yourMessage"></textarea>
+          </div>
+          <button class="modalBackground__modal__form__button">Envoyer</button>
+        </form>
+      </div>
     </div>
-  </div>
     `;
 };
 
